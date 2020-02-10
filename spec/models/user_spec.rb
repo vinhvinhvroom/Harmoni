d45568039e4c9abd07b404c1a57553d53f670531
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-09 19:27:45 -0500

    updated model test for user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ it { should_not have_valid(:email).when(nil, "", "zain@", "zain", "zain.com") }
 
 it { should have_valid(:encrypted_password).when("password", "123456", "I am long enough") }
 
-it { should_not have_valid(:encrypted_password).when(nil, "", "short") }
+it { should_not have_valid(:encrypted_password).when(nil, "") }
 
 it { should have_valid(:username).when("zainiac") }
 
