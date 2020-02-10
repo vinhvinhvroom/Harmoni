c10da17aa47b7ff6594d24afd12d7a3503cb03e4
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-09 22:53:13 -0500

    Created React Routes in APP.js

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ConcertsIndex from "./Concerts/ConcertsIndex"
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ConcertsIndex}/>
+          <Route exact path="/concerts" component={ConcertsIndex}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
