915ad686e94d81472658430d51adc5a5760e9ffd
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-10 16:55:31 -0500

    Created Welcome Tile.js

---
+++ app/javascript/react/components/Concerts/WelcomeTile.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const WelcomeTile = (props) => {
+  return(
+    <div className="row">
+      <div className="columns small-10 large-10">
+        <div className="welcome-tile">
+          <h4 className="welcome-text">"The combination of musical tones to produce a pleasing effect."</h4>
+          <h6>Let Harmoni connect you with new live music near you.</h6>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomeTile
