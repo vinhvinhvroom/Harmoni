2f15e74dc006dbcfbdd2764d0ac0a25f52b5b980
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-09 19:27:55 -0500

    updated factory bot creation

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -5,6 +5,11 @@ FactoryBot.define do
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    username { 'username' }
+    city { 'Boston' }
+    state { 'MA' }
+    zip { '02116' }
+
   end
 
 end
