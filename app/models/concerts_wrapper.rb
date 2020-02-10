521e7e08c74f4fbfe31b6a3c89efc2565b3c42cc
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-10 16:56:28 -0500

    Updated ConcertsWrapper.rb to handle logic to extract only info needed from external ticketmaster API

--- app/models/concerts_wrapper.rb
+++ app/models/concerts_wrapper.rb
@@ -1,4 +1,4 @@
-class ConcertsWrapper < ApplicationController
+class ConcertsWrapper
   BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=#{ENV["TICKET_KEY"]}"
 
   attr_reader :concerts_objects
@@ -10,12 +10,26 @@ class ConcertsWrapper < ApplicationController
   def self.retrieve_concerts(query)
     concertsData = concerts_request(query)
     concerts = concerts(concertsData)
-    ConcertsWrapper.new(concerts)
+    concerts_array=[]
+    concerts_wrapper = ConcertsWrapper.new(concerts).concerts_objects
+
+    concerts_wrapper.each do |concert|
+      concert_object = {
+        name: concert["name"],
+        date: Time.parse(concert["dates"]["start"]["localDate"]).strftime("%B %d, %Y - %I:%M%P"),
+        image: concert["images"][0]["url"],
+        url: concert["url"],
+        venue: concert["_embedded"]["venues"][0]["name"],
+        tm_id: concert["id"]
+      }
+      concerts_array << concert_object
+    end
+    return concerts_array
   end
 
   def self.concerts_request(query)
-    parsed_response = JSON.parse(Faraday.get("#{BASE_URL}&keyword=#{query}").env["response_body"])
-    return parsed_response
+    parsed_response = JSON.parse(Faraday.get("#{BASE_URL}&city=#{query}").env["response_body"])
+    return parsed_response["_embedded"]["events"]
   end
 
   def self.concerts(parsed_concerts_array)
