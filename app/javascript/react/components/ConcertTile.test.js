61ebd1042298b5b61e289875876b62cb416cb27f
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-10 22:38:40 -0500

    concert tile test

--- app/javascript/react/components/ConcertTile.test.js
+++ app/javascript/react/components/ConcertTile.test.js
@@ -12,11 +12,17 @@ describe("ConcertTile", () => {
   beforeEach(() => {
     concert = {
     "name": "Guns N' Roses 2020 Tour",
-    "date": "2020-07-21",
+    "date": "July 21, 2020 ",
     "image": "https://s1.ticketm.net/dam/a/26a/a1c6b081-0e43-4660-b28d-c1e5a145826a_1277601_RETINA_LANDSCAPE_16_9.jpg",
     "url": "http://www.ticketsnow.com/InventoryBrowse/TicketList.aspx?PID=2923458",
     "venue": "Fenway Park",
-    "tm_id": "Z7r9jZ1Ae0Nob"
+    "tm_id": "Z7r9jZ1Ae0Nob",
+    "city": "Boston",
+    "state": "MA",
+    "address": "4 Yawkey Way",
+    "genre": "Rock",
+    "sub_genre": "Rock",
+    "sale_date": "February 07, 2020 - 05:00pm"
     }
 
     wrapper = mount(
@@ -39,10 +45,10 @@ describe("ConcertTile", () => {
   });
 
   it("should render a date class", () =>{
-  expect(wrapper.find('.date').text()).toBe("2020-07-21")
+  expect(wrapper.find('.date').text()).toBe("July 21, 2020 ")
   });
 
   it("should render a venue class", () =>{
-  expect(wrapper.find('.venue').text()).toBe("Fenway Park")
+  expect(wrapper.find('.venue').text()).toBe("Fenway Park - Boston, MA")
   });
 })
