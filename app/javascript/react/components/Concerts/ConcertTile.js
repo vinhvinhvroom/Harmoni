e1cc26e06db5d2b90f10bc1bee069770dad9f352
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-10 22:38:58 -0500

    concert tile refactor

--- app/javascript/react/components/Concerts/ConcertTile.js
+++ app/javascript/react/components/Concerts/ConcertTile.js
@@ -2,16 +2,16 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const ConcertTile = ({ concert }) => {
-  let { name, date, image, url, venue } = concert
+  let { name, date, image, url, venue, city, state, address } = concert
 
   return (
-    <div className="columns small-10 large-6">
+    <div className="columns small-10 large-4 tile-wrapper">
       <div className="concert-tile">
           <img className="tile-img" src={image} />
           <div className="tile-info-wrapper">
             <h4 className="concert-name">{name}</h4>
             <h6 className="date">{date}</h6>
-            <h6 className="venue">{venue}</h6>
+            <h6 className="venue">{venue} - {city}, {state}</h6>
           </div>
       </div>
     </div>
