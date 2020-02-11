07bac4844533775fa2cf0cd5fbb1f01cc1fe80df
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-11 14:16:48 -0500

    Concert Show Container React component created

--- app/javascript/react/components/Concerts/ConcertShowContainer.js
+++ app/javascript/react/components/Concerts/ConcertShowContainer.js
@@ -1,9 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import ConcertShow from "./ConcertShow.js"
 
 const ConcertShowContainer = (props) => {
+  const[concert, setConcert] = useState([])
+
+  let concertId = props.match.params.id
+
+  useEffect(() => {
+    fetch(`/api/v1/concerts/${concertId}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        const error = new Error(`${response.status}: ${response.statusText}`);
+        throw(error)
+      }
+    })
+    .then(body => {
+      setConcert(body)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }, [])
+
   return(
     <div>
-      Hi
+      <ConcertShow
+        key={concert.id}
+        concertObject={concert}
+        />
     </div>
   )
 }
