c759f7a91de987f2484cdf4d155d79d83cfa9dab
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-11 14:17:38 -0500

    Created a route for show component in rake routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'homes#index'
 
   get "/concerts", to: "homes#index"
+  get "/concerts/:id", to: "homes#index"
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
