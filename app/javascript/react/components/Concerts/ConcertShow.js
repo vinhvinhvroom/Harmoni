06d7f0687fa3806ef0d748258c8856ec382f058f
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-13 16:22:21 -0500

    Refactored ConcertShow.js

--- app/javascript/react/components/Concerts/ConcertShow.js
+++ app/javascript/react/components/Concerts/ConcertShow.js
@@ -8,7 +8,7 @@ const ConcertShow = ({ concertObject, playlistObject, artistObject }) => {
     <div className="columns small-8 medium-6 large-4 show-wrapper" >
       <div className="show-tile">
         <img className="show show-img" src={image}/>
-          <div className="show show-content-wrapper">
+          <div className="columns small-10 medium-6 large-4 show show-content-wrapper">
             <div className="show info-wrapper">
               <h4 className="show show-name">{name}</h4>
               <p className="show show-genre">{genre}</p>
@@ -17,7 +17,7 @@ const ConcertShow = ({ concertObject, playlistObject, artistObject }) => {
               <h6 className="show show-address">{address}, {city}, {state}</h6>
               <h6 className="show show-sale-date">On Sale: {sale_date}</h6>
               <a href={url} target="_blank" className="show-button">Tickets</a>
-              <a href={artistObject} target="_blank" className="show-button">Spotify</a>
+              <a href={playlistObject} target="_blank" className="show-button">Spotify</a>
             </div>
             <div className="show spotify-player">
               <SpotifyPlayer
