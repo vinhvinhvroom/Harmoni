96312e0802512bd6fdf0a72f843af65c6f0a630c
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-13 11:56:50 -0500

    Updated TracksWrapper -- added a new .artist_search method

--- app/models/tracks_wrapper.rb
+++ app/models/tracks_wrapper.rb
@@ -7,10 +7,16 @@ class TracksWrapper
       @tracks_objects = tracks_objects
   end
 
-  def self.search(artist)
-    track = RSpotify::Playlist.search(artist)
+  def self.search(playlist)
+    track = RSpotify::Playlist.search(playlist)
     artist_playlist = track[0].external_urls["spotify"]
     return artist_playlist
+  end
+
+  def self.artist_search(artist)
+    artist = RSpotify::Artist.search(artist)
+    artist_page = artist[0].external_urls["spotify"]
+    return artist_page
   end
 
 end
