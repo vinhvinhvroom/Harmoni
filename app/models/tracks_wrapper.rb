7ef271da664a7d47d182ce2b6b7ea289e1db6d6e
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-13 16:23:09 -0500

    Created related artists method to find similar artists and add to API endpoint

--- app/models/tracks_wrapper.rb
+++ app/models/tracks_wrapper.rb
@@ -7,8 +7,8 @@ class TracksWrapper
       @tracks_objects = tracks_objects
   end
 
-  def self.search(playlist)
-    track = RSpotify::Playlist.search(playlist)
+  def self.search(artist)
+    track = RSpotify::Playlist.search(artist)
     artist_playlist = track[0].external_urls["spotify"]
     return artist_playlist
   end
@@ -17,6 +17,31 @@ class TracksWrapper
     artist = RSpotify::Artist.search(artist)
     artist_page = artist[0].external_urls["spotify"]
     return artist_page
+  end
+
+  def self.related_artists(artist)
+    artist = RSpotify::Artist.search(artist)
+
+    if artist[0] == nil
+      return nil
+    else
+      artist_name = artist[0].name
+      id = artist[0].id
+
+      response = RSpotify.get("artists/#{id}/related-artists")["artists"]
+
+      related_artists = []
+      response.each do |artist|
+        related_artist_object = {
+          name: artist["name"],
+          spotify_id: artist["id"],
+          spotify_link: artist["external_urls"]["spotify"]
+        }
+
+        related_artists << related_artist_object
+      end
+      return related_artists
+    end
   end
 
 end
