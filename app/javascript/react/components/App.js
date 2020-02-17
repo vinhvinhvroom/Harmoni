815d8d98b2ac8e67869718431bb0225986b778db
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-17 14:14:44 -0500

    created SearchIndex React Route

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ConcertsIndex from "./Concerts/ConcertsIndex"
 import ConcertShowContainer from "./Concerts/ConcertShowContainer"
+import SearchIndex from "./Concerts/SearchIndex"
 
 export const App = (props) => {
   return (
@@ -11,6 +12,7 @@ export const App = (props) => {
           <Route exact path="/" component={ConcertsIndex}/>
           <Route exact path="/concerts" component={ConcertsIndex}/>
           <Route exact path="/concerts/:id" component={ConcertShowContainer}/>
+          <Route exact path="/search" component={SearchIndex}/>
         </Switch>
       </BrowserRouter>
     </div>
