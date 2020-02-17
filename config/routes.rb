c55a2f3a05a05ed59833d649c617054bb5884cfb
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-17 14:16:37 -0500

    added routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/concerts", to: "homes#index"
   get "/concerts/:id", to: "homes#index"
+  get "/search", to: "homes#index"
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -12,14 +13,8 @@ Rails.application.routes.draw do
         resources :comments, only: [:index, :create]
         resources :tracks, only: [:show]
       end
-        post '/concerts/search', to: 'concerts#search'
+        post '/tracks/search', to: 'tracks#search'
     end
   end
-
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :comments, only: [:index]
-  #   end
-  # end
 
 end
