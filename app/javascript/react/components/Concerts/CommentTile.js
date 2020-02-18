709ebce0eab8841b49ae315c33af3588c5487d0f
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-18 08:49:44 -0500

    Added classNames

--- app/javascript/react/components/Concerts/CommentTile.js
+++ app/javascript/react/components/Concerts/CommentTile.js
@@ -8,9 +8,9 @@ const CommentTile = ({ commentData }) => {
       <li>
         <div className="columns small-12, medium-10, large-10 comments-all-wrapper">
           <div className="comment-tile-wrapper">
-            <h5>{user_name}</h5>
-            <p>{comment}</p>
-            <p>{comment_time}</p>
+            <h5 className="comment-text">{user_name}</h5>
+            <p className="comment-text">{comment}</p>
+            <p className="comment-text">{comment_time}</p>
           </div>
         </div>
       </li>
