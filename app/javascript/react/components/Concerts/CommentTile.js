5986b79df12d9dcc9711308fe6c5033e71993511
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-18 11:26:01 -0500

    add classname

--- app/javascript/react/components/Concerts/CommentTile.js
+++ app/javascript/react/components/Concerts/CommentTile.js
@@ -4,7 +4,7 @@ const CommentTile = ({ commentData }) => {
   let { comment, user_name, comment_time } = commentData
 
   return(
-    <div className="row">
+    <div className="row comment-tile-container">
       <li>
         <div className="columns small-12, medium-10, large-10 comments-all-wrapper">
           <div className="comment-tile-wrapper">
