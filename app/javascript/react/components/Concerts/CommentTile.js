0491a2caae179d649b847632182e5043e12ea76a
vinhvinhvroom <vinh.doan13@gmail.com>
2020-02-19 16:50:40 -0500

    Created front end delete

--- app/javascript/react/components/Concerts/CommentTile.js
+++ app/javascript/react/components/Concerts/CommentTile.js
@@ -1,33 +1,11 @@
 import React, { useState } from "react"
 import { Redirect } from "react-router-dom"
 
-const CommentTile = ({ commentData, concertId }) => {
+const CommentTile = ({ commentData, concertId, deleteComment }) => {
   let { id, comment, user_name, user, comment_time, tm_id } = commentData
 
-  const deleteComment = () => {
-  fetch(`/api/v1/concerts/${concertId}/comments/${id}`, {
-    credentials: "same-origin",
-    method: 'DELETE',
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    }
-  })
-  .then(response => {
-    if (response.ok) {
-      return response;
-    } else {
-      alert(response.message)
-      let errorMessage = `${response.status} (${response.statusText})`,
-       error = new Error(errorMessage)
-      throw error
-    }
-  })
-    .then(response => response.json())
-    .then(body => {
-      alert(body.message)
-    })
-    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  const handleDelete = () => {
+    deleteComment(id)
   }
 
 
@@ -39,7 +17,7 @@ const CommentTile = ({ commentData, concertId }) => {
             <h5 className="comment-text">{user_name}</h5>
             <p className="comment-text">{comment}</p>
             <p className="comment-text">{comment_time}</p>
-            <input onClick={deleteComment} className="button delete-comment" type="submit" value="Delete Comment"/>
+            <input onClick={handleDelete} className="button delete-comment" type="submit" value="Delete Comment"/>
           </div>
         </div>
       </li>
